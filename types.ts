d1da82528112c0645f3d7a98e04cfac811deeede
Abdul Mubeen <abdulmubeen57@gmail.com>
2023-07-30 09:52:33 +0530

    Developed Featured Page

--- types.ts
+++ types.ts
@@ -9,3 +9,31 @@ export interface Category {
   name: String;
   billboard: Billboard;
 }
+
+export interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  size: Size;
+  color: Color;
+  images: Image[];
+}
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Color {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
